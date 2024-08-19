f8e8b60a7d007e3c5364b4dd3647a5d4a77254a8
ashfaq <khanashfaq9423@gmail.com>
2024-08-19 20:18:37 +0530

    complete react food app

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-import './placeOrder.css'
+import React, { useContext } from "react";
+import "./placeOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 function PlaceOrder() {
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <form action="" className="place-order">
+        <div className="place-order-left">
+          <p className="title">Delivery Information</p>
+          <div className="multi-fields">
+            <input type="text" placeholder="First name" />
+            <input type="text" placeholder="Last name" />
+          </div>
+          <input type="text" placeholder="Email address" />
+          <input type="text" placeholder="Street" />
+          <div className="multi-fields">
+            <input type="text" placeholder="city" />
+            <input type="text" placeholder="State" />
+          </div>
+          <div className="multi-fields">
+            <input type="text" placeholder="Zip code" />
+            <input type="text" placeholder="country" />
+          </div>
+          <input type="text" placeholder="Phone" />
+        </div>
+        <div className="place-order-right">
+          <div className="cart-total">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>${getTotalCartAmount()}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                {" "}
+                <p>Delivery Fee</p>
+                <p>${2}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                {" "}
+                <p>Total</p>
+                <p>${getTotalCartAmount() + 2}</p>
+              </div>
+            </div>
+            <button> PROCEED TO PAYMENT </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default PlaceOrder
+export default PlaceOrder;
